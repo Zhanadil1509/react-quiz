6ae5e32d0eb46c4bcfe02f9f9b7ab4c37257392a
Zhanadil <zhanadil1509@gmail.com>
2019-09-11 21:25:58 +0600

    get test list from server

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 import {QuizLists} from "./style";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 class QuizList extends Component {
 
-    renderQuizes = () => [1, 2, 3].map((v, i) => {
+    state = {
+        quizes: []
+    }
+
+    renderQuizes = () => this.state.quizes.map( v => {
         return (
-            <li key={i}>
-                <NavLink to={`/quiz/${v}`}>Тест {v}</NavLink>
+            <li key={v.id}>
+                <NavLink to={`/quiz/${v.id}`}>{v.name}</NavLink>
             </li>
         )
     })
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('https://react-quiz-81546.firebaseio.com/quizes.json')
+            const quizes = []
+            Object.keys(response.data).forEach((v, i) => {
+                quizes.push({
+                    id: v,
+                    name: `Test #${i + 1}`
+                })
+            })
+            this.setState({quizes})
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     render() {
         return (
