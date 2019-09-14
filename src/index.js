2dea6e1f89c08513cf16ae096ea3b691a77befa5
Zhanadil <zhanadil1509@gmail.com>
2019-09-14 10:21:19 +0600

    i redux, redux-thunk, react-redux, connect devtools

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Routes from './Routes'
-import {BrowserRouter as Router } from "react-router-dom";
+import {BrowserRouter as Router } from "react-router-dom"
+import {applyMiddleware, compose, createStore} from "redux";
+import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<Router>
-    <Routes />
-</Router>, document.getElementById('root'))
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)) )
+
+ReactDOM.render(<Provider store={store}>
+    <Router>
+        <Routes />
+    </Router>
+</Provider>, document.getElementById('root'))
